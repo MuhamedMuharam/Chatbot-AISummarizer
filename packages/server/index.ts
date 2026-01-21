a3f6a102ee2a323dc44658a10e093edfb79993df
Muhamed <muhamedmuharam1411@gmail.com>
2026-01-21 20:13:44 +0200

    Connect FE and BE

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,14 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, World!');
 });
+app.get('/api/hello', (req: Request, res: Response) => {
+    res.json({ message: 'Hello from the API!' });
+});
+
+app.get('/api/hello2', (req: Request, res: Response) => {
+    res.json({ message: 'Hello from the 2!' });
+});
+
 
 app.listen(port, () => {  //starts our web server
     console.log(`Server is running on http://localhost:${port}`);
